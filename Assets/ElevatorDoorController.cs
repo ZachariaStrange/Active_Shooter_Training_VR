76210950bd60b244e0845cb9a7751dac2afdb1a8
ZachariaStrange <zstran5871@gmail.com>
2024-10-14 14:01:50 -0400

    Example code to make sure it works

--- Assets/ElevatorDoorController.cs
+++ Assets/ElevatorDoorController.cs
@@ -7,7 +7,7 @@ public class ElevatorDoorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("wow");
     }
 
     // Update is called once per frame
