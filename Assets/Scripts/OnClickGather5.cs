908490a354936a7b59032554fa362007135241f2
Anna Budreau <akbudreau@gmail.com>
2024-12-08 17:51:37 -0500

    finished first hide animation
    all 5 boys move to front of the room as teacher "gathers" them to talk to them

---
+++ Assets/Scripts/OnClickGather5.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickGather5 : MonoBehaviour
+{
+    public Animator animator;
+    public float moveSpeed = 1f;
+    public UnityEngine.Vector3 targetPosition;
+    private bool isWalking = false;
+    public Button exitHide1;
+
+
+
+    public void Start()
+    {
+        exitHide1.onClick.AddListener(GoToTarget);
+    }
+    // Update is called once per frame
+    public void Update()
+    {
+        if(isWalking)
+        {
+            MoveCharacter();
+        }
+    }
+
+    public void GoToTarget()
+    {
+        UnityEngine.Vector3 target = new UnityEngine.Vector3(0.25f, 0f, 3f);
+        targetPosition = target;
+        isWalking = true;
+        animator.SetBool("move", true); //set animation to walk (aka move)
+    }
+
+    public void MoveCharacter()
+    {
+        transform.position = UnityEngine.Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        if(transform.position == targetPosition)
+        {
+            isWalking = false;
+            animator.SetBool("move", false);
+        }
+    }
+}
